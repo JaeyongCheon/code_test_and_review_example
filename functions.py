8572692d355cb48e05a8679fa51d5595d239d4e8
Ohjun Kwon <seanieskb@gmail.com>
2025-10-15 19:12:11 +0900

    fix: resolve merge conflict and add divide(a, b)

--- functions.py
+++ functions.py
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 def divide(a, b):
 		pass
+=======
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def minus(a, b):
+    """Subtract b from a."""
+    return a - b
+
+def square(a, b):
+    return a**b
+
+def divide(a, b):
+    """Divide a by b."""
+    pass
+    
+>>>>>>> 7ccd339 (fix: resolve merge conflict and add divide(a, b))
