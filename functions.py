119d6de21990c434213846081174ac6092f20787
seanieskb-ux <seanieskb@gmail.com>
2025-10-15 18:55:45 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,2 @@
-def add(a, b):
-    """Add two numbers."""
-    return a + b
-
-def minus(a, b):
-    """Subtract b from a."""
-    return a - b
-
-def square(a, b):
-    return a**b
-	
 def divide(a, b):
 		pass
