ae378f1d4284b2a0ed388f927b0ceb254b731bea
bynminsuk-creator <bynminsuk@gmail.com>
2025-10-26 12:45:18 +0900

    feat: add root() function
    제곱근 계산 기능 추가

--- functions.py
+++ functions.py
@@ -5,3 +5,10 @@ def add(a, b):
 def minus(a, b):
     """Subtract b from a."""
     return a - b
+
+def root(x: float) -> float:
+    """주어진 수의 제곱근을 반환합니다."""
+    if x < 0:
+        raise ValueError("음수는 제곱근을 계산할 수 없습니다.")
+    return x ** 0.5
+
